d154aa8de49f8d52fb4b50f9879864ee741e2cd1
Oliver Jones <olijones44@hotmail.co.uk>
2023-03-11 11:16:12 +0000

    Update logic, add reset button

--- main.js
+++ main.js
@@ -1,22 +1,121 @@
 import './style.css';
 
-// Set ID elements
+// Set option elements
 const slider = document.getElementById('lengthSelector');
-const length = document.getElementById('length');
-const output = document.getElementById('output');
-const generate = document.getElementById('generate');
-const copyClipboard = document.getElementById('clipboard');
-
-// Checkbox ID elements
 const uppercase = document.getElementById('uppercase');
 const lowercase = document.getElementById('lowercase');
 const numbers = document.getElementById('numbers');
 const specials = document.getElementById('specials');
+const resetButton = document.getElementById('reset');
+
+// Set data elements
+const length = document.getElementById('length');
+const output = document.getElementById('output');
+const generate = document.getElementById('generate');
+const copyClipboard = document.getElementById('clipboard');
+const copyText = document.getElementById('output');
+const copiedText = document.getElementById('copied');
+const footerText = document.getElementById('footer');
 
 // Strength boxes
 const strengthBoxes = document.getElementsByClassName('strength-box');
 
-// Options control
+// Function for updating strength
+
+function setTransparent(index) {
+	for (let i = index; i < strengthBoxes.length; i++) {
+		strengthBoxes[i].style.backgroundColor = 'transparent';
+	}
+}
+
+function updateStrength() {
+	resetButton.style.display = 'block';
+	resetButton.style.visibility = 'visible';
+
+	let passwordScore = 0;
+
+	if (selections() === 0) {
+		passwordScore = 0;
+	} else if (selections() === 1) {
+		passwordScore = 10;
+	} else if (selections() === 2) {
+		passwordScore = 20;
+	} else if (selections() === 3) {
+		passwordScore = 30;
+	} else if (selections() === 4) {
+		passwordScore = 40;
+	}
+
+	if (Number(length.innerText) >= 15) {
+		passwordScore += 20;
+	} else {
+		passwordScore += 10;
+	}
+
+	if (passwordScore >= 10) {
+		strengthBoxes[0].style.backgroundColor = '#F60707';
+		setTransparent(1);
+	}
+	if (passwordScore >= 20) {
+		strengthBoxes[1].style.backgroundColor = '#F67207';
+		setTransparent(2);
+	}
+	if (passwordScore >= 30) {
+		strengthBoxes[2].style.backgroundColor = '#F6A707';
+		setTransparent(3);
+	}
+	if (passwordScore >= 40) {
+		strengthBoxes[3].style.backgroundColor = '#F6DC07';
+		setTransparent(4);
+	}
+	if (passwordScore >= 50) {
+		strengthBoxes[4].style.backgroundColor = '#B1F607';
+		setTransparent(5);
+	}
+	if (passwordScore >= 60) {
+		strengthBoxes[5].style.backgroundColor = '#56F607';
+		setTransparent(6);
+	}
+}
+
+// Listeners
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+
+checkboxes.forEach((checkbox) => {
+	checkbox.addEventListener('change', function () {
+		setDefaultMessage();
+		updateStrength();
+	});
+});
+
+resetButton.addEventListener('click', function () {
+	setDefaultMessage();
+	resetButton.style.display = 'none';
+	resetButton.style.visibility = 'hidden';
+	length.innerHTML = '15';
+	checkboxes.forEach((checkbox) => {
+		checkbox.checked = false;
+	});
+
+	slider.value = '15';
+	setTransparent(0);
+});
+
+// Set footer
+function footer() {
+	const year = new Date().getFullYear();
+
+	footerText.innerText = `Jonesy Creations 😎 ${year}`;
+}
+
+footer();
+
+// Messages
+const messages = {
+	default: 'Click generate!',
+	noOptions: 'No options selected!',
+	copied: 'Password copied!',
+};
 
 // Slider control
 
@@ -24,22 +123,44 @@ length.innerHTML = slider.value;
 
 slider.oninput = function () {
 	length.innerHTML = this.value;
+	updateStrength();
 };
 
 // Password default output
 
-output.innerHTML = 'Click generate!';
+function setDefaultMessage() {
+	output.innerHTML = messages.default;
+}
+
+setDefaultMessage();
+
+// Validation
+
+function selections() {
+	return (
+		uppercase.checked + lowercase.checked + numbers.checked + specials.checked
+	);
+}
+
+// Alert function
+
+function reset() {
+	copiedText.style.visibility = 'hidden';
+	copiedText.style.display = 'none';
+}
+
+function alertUser(msg, time) {
+	copiedText.innerHTML = msg;
+	copiedText.style.visibility = 'visible';
+	copiedText.style.display = 'block';
+	setTimeout(() => reset(), time);
+}
 
 //  Copy to clipboard
 
 copyClipboard.addEventListener('click', () => {
-	let copyText = document.getElementById('output');
-	let copiedText = document.getElementById('copied');
-
-	if (copyText.innerText === 'Click generate!') {
-		copiedText.innerHTML = 'No options selected!';
-		copiedText.style.visibility = 'visible';
-		setTimeout(() => (copiedText.style.visibility = 'hidden'), 3000);
+	if (selections() === 0) {
+		alertUser(messages.noOptions, 3000);
 	} else {
 		// Can't copy to clipboard without input tag so using
 		// hidden input tag to store the text
@@ -48,15 +169,14 @@ copyClipboard.addEventListener('click', () => {
 
 		// Select the text field in the hidden input element
 		hiddenField.select();
-		hiddenField.setSelectionRange(0, 99999); // For mobile devices
+		// For mobiles
+		hiddenField.setSelectionRange(0, 99999);
 
 		// Copy the text inside the text field
 		navigator.clipboard.writeText(hiddenField.value);
 
 		// Show temporary div
-		copiedText.innerHTML = 'Password copied!';
-		copiedText.style.visibility = 'visible';
-		setTimeout(() => (copiedText.style.visibility = 'hidden'), 5000);
+		alertUser(messages.copied, 5000);
 	}
 });
 
@@ -66,6 +186,11 @@ generate.addEventListener('click', generatePassword);
 
 function generatePassword() {
 	const passwordLength = Number(length.innerText);
+
+	if (selections() === 0) {
+		alertUser(messages.noOptions, 3000);
+		return;
+	}
 
 	const checked = [
 		{
@@ -86,7 +211,7 @@ function generatePassword() {
 		{
 			type: 'special',
 			checked: specials.checked,
-			chars: ['?', '!', '%', '$'],
+			chars: ['?', '!', '%', '$', '@', '(', ')'],
 		},
 	];
 
@@ -117,9 +242,6 @@ function getRandInt(x) {
 function getPassword(length, checked) {
 	let newPassword = '';
 	let charArrayGroup = [];
-
-	// TODO: set up score system for strength boxes
-	// let passwordScore = 0;
 
 	// TODO: change logic so it always includes checked items
 
